08e2013b8f7aa0c1b06eb11a1097ffc4d35debe7
Mohith R Gowda <eng25am0073@dsu.edu.in>
2026-05-17 22:44:30 +0530

    Update mohith2.c

--- mohith2.c
+++ mohith2.c
@@ -7,15 +7,13 @@
 int main() {
     int n, sum = 0;
 
-    // Ask the user for input
+   
     printf("Enter a positive integer: ");
     scanf("%d", &n);
 
-    // Calculate sum using a loop
     for (int i = 1; i <= n; i++) {
         sum += i;
 
-    // Display the result
     printf("The sum of the first %d natural numbers is: %d\n", n, sum);
 
     return 0;
