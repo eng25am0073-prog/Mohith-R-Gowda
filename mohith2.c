3a562c624e457cc3257f1b4ffca27ca008d03dff
Mohith R Gowda <eng25am0073@dsu.edu.in>
2026-09-03 23:45:20 +0530

    Update mohith2.c

--- mohith2.c
+++ mohith2.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
-int main() {
+int main()
+ {
     int n, sum = 0;
    
     printf("Enter a positive integer: ");//hehe
     scanf("%d", &n);
-    for (int i = 1; i <= n; i++) {
+    for (int i = 1; i <= n; i++) 
+{
         sum += i;
+}
     printf("The sum of the first %d natural numbers is: %d\n", n, sum);
     return 0;
 }
